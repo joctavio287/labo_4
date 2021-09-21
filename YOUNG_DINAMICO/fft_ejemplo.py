2d32e2064c1718c72296353dbb0baa8df249645e
joctavio287 <joctavio287@gmail.com>
2021-09-21 17:20:45 -0300

    probando cositas

--- YOUNG_DINAMICO/fft_ejemplo.py
+++ YOUNG_DINAMICO/fft_ejemplo.py
@@ -14,7 +14,7 @@ t = np.arange(0, tmax, tstep)
 # Una función de prueba.
 f0, f1 = 7, 20
 
-V = 2*np.sin(2*np.pi*f0*t) + np.sin(2*np.pi*f1*t) 
+V = 2*np.sin(2*np.pi*f0*t) + 1*np.sin(2*np.pi*f1*t) 
 
 # Graficamos la función de prueba
 with plt.style.context('seaborn-whitegrid'):
@@ -28,37 +28,77 @@ with plt.style.context('seaborn-whitegrid'):
 # Hacemos la trasnformada de fourier (FFT) y graficamos.
 fft = np.fft.fft(V)
 
-# Calular el vector de frecuencias.
-frecs = np.linspace(0, fsamp/2, int(N/2))
+# Calular el vector de frecuencias. Se calcula hasta la mitad porque
+# el algoritmo espeja los valores (para entender descomentar frecs2
+# y las lineas dentro del plot).
+
+frecs = np.linspace(0, fsamp/2, int(N/2)) 
+# frecs2 = np.linspace(0, fsamp, N)
 
 # Graficamos la tasnformada de fourier
 with plt.style.context('seaborn-whitegrid'):
     fig, ax = plt.subplots(nrows = 1, ncols = 1, figsize = (10, 5))
-    ax.plot(frecs, 2*np.abs(fft[:N//2])/N) # PREGUNTAR POR QUE HAY QUE NORMALIZAR CON UN MEDIO
+    # PREGUNTAR POR QUE HAY QUE NORMALIZAR CON UN MEDIO. Es algo del teo del muestreo,
+    # como que para definir una seña periódica necesitas como minimos dos puntos
+    ax.plot(frecs, 2*np.abs(fft[:N//2])/N) 
+    # ax.plot(frecs2, 2*np.abs(fft)/N) 
+    # ax.set_xlim([0,1000])
     ax.set_xlim([0, 25])
-    ax.set_ylabel('Potencia')
+    ax.set_ylabel('Amplitud espectral')
     ax.set_xlabel('Frecuencia [Hz]')
     plt.tight_layout()
     plt.show()
 
  
 # Función para graficar transformada
-def tirafft(señal, f_samp, formula):
+def tirafft(señal, f_samp, formula:str = 'la señal', log = False, picos = True, prominence = 1):
+    '''
+    INPUT: 
+    señal: señal de entrada, preferentemente en formato np.array().
+    f_samp: es la frecuencia de sampleo de la señal. i.e: 1/pasotemporal.
+    formula: es una expresión en latex para printear en el gráfico.
+    log: si la transformada la hace en escala logarítmica. Esto resulta conveniente
+    para encontrar picos de frecuencias que tienen amplitud relativa muy baja.
+    picos: si buscar los picos de la FFT. Es útil, primero tomarla por False, para poder
+    verla en todo el rango y chequear, después, si al correrla en True no se pierden picos.
+    prominence: es la altura del menor pico, si se pasan dos valores, el segundo se 
+    interpreta como el maximo valor.
+    '''
     señal_fft, N = np.fft.fft(señal), len(señal)
     xf = np.linspace(0, f_samp/2, int(N/2))
     yf = 2*np.abs(señal_fft[:N//2])/N
-    picos_x, intensidad_picos = find_peaks(yf, height = 1)
+    if picos == True:
+        picos_x, intensidad_picos = find_peaks(yf, prominence = prominence)
     with plt.style.context('seaborn-whitegrid'):
         fig, ax = plt.subplots(nrows = 1, ncols = 1, figsize = (10, 5))
         ax.plot(xf, yf, label = r'Transformada de Fourier de {}'.format(formula))
-        ax.set_xlim(0, xf[picos_x[-1]] + 1)
-        ax.set_ylabel('Potencia')
+        if log == False:
+            ax.set_ylabel('Amplitud espectral')
+        else:
+            ax.set_ylabel('Amplitud espectral (escala logaritmica)')
+            ax.set_yscale('log')
+        if picos == True:
+            ax.set_xlim(0, xf[picos_x[-1]] + 1)
+            # for x_p, y_p in zip([xf[int(x)] for x in picos_x],  [yf[xf[int(x)]] for x in picos_x]):
+            #     ax.plot(x_p, y_p,
+            #      label = 'Pico de amplitud: ({},{})'.format(np.round(x_p, 2), np.round(y_p, 2)))
+        else: 
+            ax.set_xlim(0, f_samp/2)
         ax.set_xlabel('Frecuencia [Hz]')
         # ax.set_title(r'FFT de {}'.format(formula))
         # ax.text(1, 1, 'picos en: {} Hz'.format(np.round(xf[picos_x], 2)), fontsize = 15)
         ax.legend(fontsize = 15)
         plt.tight_layout()
         plt.show()
-    return xf[picos_x], intensidad_picos['peak_heights']
+    # if picos == True:
+    #     # return [xf[int(x)] for x in picos_x], [yf[xf[int(x)]] for x in picos_x]
 
-picos, altura =tirafft(V, fsamp, str(r'$sin(2\, \pi\, {} \, t) +sin(2\, \pi\, {} \, t)$'.format(f0,f1)))
+picos, alturas = tirafft(V, fsamp, str(r'$sin(2\, \pi\, {} \, t) +sin(2\, \pi\, {} \, t)$'.format(f0,f1)))
+V2 = 2*np.sin(2*np.pi*f0*t) + .001*np.sin(2*np.pi*f1*t) 
+picos, altura = tirafft(V2, fsamp, log = True, prominence =.0001)
+picos
+señal = 2*np.sin(2*np.pi*f0*t) + .001*np.sin(2*np.pi*f1*t)
+señal_fft, N = np.fft.fft(señal), len(señal)
+xf = np.linspace(0, fsamp/2, int(N/2))
+yf = 2*np.abs(señal_fft[:N//2])/N
+picos_x, intensidad_picos = find_peaks(yf, prominence = .0001)
