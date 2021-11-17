1f02792c2f7c0e9658fd0083fb2b1d98f876746e
joctavio287 <joctavio287@gmail.com>
2021-11-17 13:33:22 -0300

    agregue análisis de los datos

--- FERROMAGNETISMO/medir.py
+++ FERROMAGNETISMO/medir.py
@@ -1,7 +1,7 @@
-import time, pyvisa, numpy as np, matplotlib.pyplot as plt
+import time, pyvisa, numpy as np, matplotlib.pyplot as plt, pandas as pd
 
 # ========================================================================
-# Chequeo los instrumentos que están conectados
+# Chequeamos los instrumentos que están conectados
 # ========================================================================
 
 rm = pyvisa.ResourceManager()
@@ -24,7 +24,7 @@ osc.write(f'CH{1}:SCale {escala:3.1E}')
 osc.write(f'CH{2}:SCale {escala:3.1E}')
 
 # ========================================================================
-# Seteo la escala horizontal teniendo en cuenta que la frecuencia del toma
+# Setemos la escala horizontal teniendo en cuenta que la frecuencia del toma
 # (220 V) es de 50 Hz.
 # ========================================================================
 freq = 50 # Hz
@@ -37,16 +37,16 @@ osc.write(f'HORizontal:MAin:SCale {escala_t:3.1E}')
 # que coincidan'.
 # ========================================================================
 
-# Las tres magnitudes que vamos a medir:
+# Las tres magnitudes que vamos a medir
 resistencia, tension_1, tension_2 = [], [], []
 
-# Las marcas temporales de las mediciones:
+# Las marcas temporales de las mediciones
 marca_temporal_resistencia, marca_temporal_tension_1, marca_temporal_tension_2 = [], [], []
 
-# Las mediciones se van a efectuar cada 'intervalo_temporal' s:
+# Las mediciones se van a efectuar cada 'intervalo_temporal'
 intervalo_temporal = 5
 
-# Hay que hacer una iteración para saber cuánto tarda y podamos asignar bien el intervalo temporal:
+# Hay que hacer una iteración para saber cuánto tarda y podamos asignar bien el intervalo temporal
 t_auxiliar_1 = time.time()
 
 # ESTA ES UNA ITERACION PELADA
@@ -66,8 +66,8 @@ marca_tension_2 = t + (time.time() - t)/2
 t - marca_tension_2
 t_auxiliar_2 = time.time()
 
-# Actualizo el valor del intervalo temporal (resto de manera tal que el tiempo del inervalo 
-# resultante sea el especificado más arriba):
+# Actualizamos el valor del intervalo temporal (resto de manera tal que el tiempo del inervalo 
+# resultante sea, en verdad, el especificado más arriba):
 intervalo_temporal -= t_auxiliar_2 - t_auxiliar_1
 
 # Asumimos que el fenómeno dura 3.5'= 210'', modificar de ser necesario
@@ -86,7 +86,7 @@ for i in iteraciones:
     resistencia.append(float(mult.query('MEASURE:FRES?')))
     # La marca temporal es el promedio entre antes y después de medir
     marca_resistencia = t + (time.time() - t)/2
-    # Appendeo el tiempo respecto al t_0
+    # Appendeamos el tiempo respecto al t_0
     marca_temporal_resistencia.append(t_0 - marca_resistencia)
 
     # Medición de tensión en el primario
@@ -95,7 +95,7 @@ for i in iteraciones:
     tension_1.append(osc.query_ascii_values('MEASUrement:IMMed:VALue?')[0])
     # La marca temporal es el promedio entre antes y después de medir
     marca_tension_1 = t + (time.time() - t)/2
-    # Appendeo el tiempo respecto al t_0
+    # Appendeamos el tiempo respecto al t_0
     marca_temporal_tension_1.append(t_0 - marca_tension_1)
 
     # Medición de tensión en el secundario
@@ -104,11 +104,29 @@ for i in iteraciones:
     tension_2.append(osc.query_ascii_values('MEASUrement:IMMed:VALue?')[0])
     # La marca temporal es el promedio entre antes y después de medir
     marca_tension_2 = t + (time.time() - t)/2
-    # Appendeo el tiempo respecto al t_0
+    # Appendeamos el tiempo respecto al t_0
     marca_temporal_tension_2.append(t_0 - marca_tension_2)
     
     # Intervalo temporal entre mediciones
     time.sleep(intervalo_temporal)
+
+# Convertimos en formato numpy los datos
+resistencia,tension_1,tension_2=np.array(resistencia),np.array(tension_1),np.array(tension_2)
+marca_temporal_resistencia,marca_temporal_tension_1,marca_temporal_tension_2=np.array(marca_temporal_resistencia),np.array(marca_temporal_tension_1),np.array(marca_temporal_tension_2)
+
+# Antes de analizar lo medido, guardamos los datos
+datos = pd.Dataframe(data = {'resistencia[Ohms]':resistencia, 'marca_temporal_resistencia' : marca_temporal_resistencia,
+                             'tension_1[V]': tension_1, 'marca_temporal_tension_1' : marca_temporal_tension_1,
+                             'tension_2[V]': tension_2, 'marca_temporal_tension_2' : marca_temporal_tension_2})
+
+# Modificar acorde a dónde guardemos en la compu del labo:
+full_path = 'C:/repos/labo_4/FERROMAGNETISMO/Mediciones/'
+
+# Cambiar después de cada medición, sino los datos se van a reescribir
+o = 0
+datos.to_csv(full_path + 'medicion_{}'.format(o))
+
+#################################### ANALISIS DE DATOS ####################################
 
 # Hacemos la conversión de resistencia [Ohms] a Temperatura [C]
 def funcion_conversora(t):
@@ -135,16 +153,92 @@ plt.legend()
 plt.show(block = False)
 
 # ATENCION NO PRINTEAR NI 'temperaturas_auxiliar', ni 'conversor', son muchos numeros y rompen la maquina.
-temperaturas_auxiliar = np.linspace(-300, 300, 20000) # Un rango de temperaturas muy fino
-# Creo un diccionario para transformar resistencias en temperaturas:
+
+# Un rango de temperaturas muy fino, se puede hacer todavía más fino si la resolución de las mediciones es muy buena.
+temperaturas_auxiliar = np.linspace(-300, 300, 100000) 
+
+# Creamod un diccionario para transformar resistencias en temperaturas:
 conversor = {r: t for r, t in zip(funcion_conversora(temperaturas_auxiliar), temperaturas_auxiliar)}
 
 # Transformamos las impedancias medidas [Ohms] a temperatura [K]
 temperatura = [conversor[min(conversor.keys(), key = lambda x:abs(x-r)) + 273.15] for r in resistencia]
+marca_temporal_temperatura = marca_temporal_resistencia
+
+# =================================================================================================
+# De alguna manera tenemos que unir las mediciones para que todas esten asociadas al mismo tiempo,
+# para ello podemos tomar alguna de las mediciones (temperatura, tension_primario o secundario) co_
+# mo y utilizar su huella temporal como referencia a partir de la cual interpolaremos el resto.
+# Tomamos la temperatura como referencia (después cambiar como sea conveniente).
+# =================================================================================================
+
+# Interpolación de la tension_1 y la tension_2 en base a la huella temporal de la temperatura
+tension_1_interpolada = np.interp(marca_temporal_temperatura, marca_temporal_tension_1, tension_1)
+error_temporal_tension_1 =  np.abs(marca_temporal_tension_1 - marca_temporal_temperatura)
+
+tension_2_interpolada = np.interp(marca_temporal_temperatura, marca_temporal_tension_2, tension_2)
+error_temporal_tension_2 =  np.abs(marca_temporal_tension_2 - marca_temporal_temperatura)
 
 
-# Unimos los datos, para eso interpolamos usando la temperatura como referencia
-x = np.linspace(0, 2*np.pi, 10)
-y = np.sin(x)
-xvals = np.linspace(0, 2*np.pi, 50)
-yinterp = np.interp(xvals, x, y)
+# =================================================================================================
+# Antes de graficar sólo resta integrar numéricamente la tensión del secundario:
+# =================================================================================================
+
+# # Si la señal tiene offset
+# tension_1_interpolada = tension_1_interpolada - tension_1_interpolada.mean()
+
+# Realizo la integral:
+tension_2_interpolada_integrada = np.cumsum(tension_2_interpolada)
+
+# =================================================================================================
+# Ahora que tenemos las tres mediciones asociadas al mismo tiempo podemos graficar los datos:
+# =================================================================================================
+
+with plt.style.context('seaborn-whitegrid'):
+    fig, ax = plt.subplots(nrows = 1, ncols = 1, figsize = (8,7))
+    # Graficamos la tensión integrada del secundario (proporcional a B) en función de la tensión del primario (proporcional a H)
+    ax.plot(, tension_2_interpolada, label = 'Curva de histeresis')
+    ax.set_xlabel(r'\propto B [UNIDADES?]')
+    ax.set_ylabel(r'\propto H [V]')
+    fig.show()
+
+
+
+# # Modelos de juguete
+
+# # Para interpolar
+# x = np.linspace(0, 2, 10)
+# y = np.sin(x)
+# xvals = np.linspace(0, 2, 3)
+# yinterp = np.interp(xvals, x, y)
+# plt.figure()
+# plt.scatter(xvals, yinterp, color = 'red', s = 5, label = 'Valores interpolados')
+# plt.scatter(x, y, color = 'black', s = 5, label = 'Valores reales')
+# plt.legend()
+# plt.show(block=False)
+
+# Para integrar
+
+# Tiempo total
+T = 10*np.pi
+
+# El factor de escala está basado en algunas simulaciones que corrí
+numero_de_puntos = int(T/0.15) # Este es un valor que da una buena integral
+
+# Datos sintéticos
+x = np.linspace(0, T, numero_de_puntos)
+y = 3*np.sin(x) 
+
+# Si los datos tienen media
+y -= y.mean()
+
+# Calculo integral numérica
+integral_y = np.cumsum(y) * (T/len(x))
+integral_real_y = -3*np.cos(x) 
+
+# Grafico
+plt.figure()
+plt.plot(x, integral_y, color = 'black', label = 'Integral numérica')
+plt.plot(x, integral_real_y, color = 'violet', label = 'Integral real')
+plt.plot(x, y, color = 'red', label = 'Funcion real')
+plt.legend()
+plt.show(block = False)
